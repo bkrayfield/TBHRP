4c3efbe064215b4f1ecedc168ff63d9cf5e9c04e
bkrayfield <blake.rayfield@nau.edu>
2023-06-06 06:19:00 -0700

    Adding a file

---
+++ Summer2023_Remake/doHRP.py
@@ -0,0 +1,82 @@
+import re
+import sys
+from sklearn.feature_extraction.text import TfidfVectorizer
+import matplotlib.pyplot as plt
+import scipy.cluster.hierarchy as sch,random,numpy as np,pandas as pd
+from os import listdir
+from os.path import isfile, join
+import math
+import pickle
+#from sklearn.covariance import LedoitWolf
+from scipy.cluster.hierarchy import ClusterWarning
+from warnings import simplefilter
+simplefilter("ignore", ClusterWarning)
+
+
+
+# On 20151227 by MLdP <lopezdeprado@lbl.gov>
+# Hierarchical Risk Parity
+#------------------------------------------------------------------------------
+def getIVP(cov,**kargs):
+    # Compute the inverse-variance portfolio
+    ivp=1./np.diag(cov)
+    ivp/=ivp.sum()
+    return ivp
+#------------------------------------------------------------------------------
+def getClusterVar(cov,cItems):
+    # Compute variance per cluster
+    cov_=cov.loc[cItems,cItems] # matrix slice
+
+    w_=getIVP(cov_).reshape(-1,1)
+    cVar=np.dot(np.dot(w_.T,cov_),w_)[0,0]
+    return cVar
+#------------------------------------------------------------------------------
+def getQuasiDiag(link):
+    # Sort clustered items by distance
+    link=link.astype(int)
+    sortIx=pd.Series([link[-1,0],link[-1,1]])
+    numItems=link[-1,3] # number of original items
+    while sortIx.max()>=numItems:
+        sortIx.index=range(0,sortIx.shape[0]*2,2) # make space
+        df0=sortIx[sortIx>=numItems] # find clusters
+        i=df0.index;j=df0.values-numItems
+        sortIx[i]=link[j,0] # item 1
+        df0=pd.Series(link[j,1],index=i+1)
+        sortIx=sortIx.append(df0) # item 2
+        sortIx=sortIx.sort_index() # re-sort
+        sortIx.index=range(sortIx.shape[0]) # re-index
+    return sortIx.tolist()
+#------------------------------------------------------------------------------
+def getRecBipart(cov,sortIx):
+    # Compute HRP alloc
+    w=pd.Series(1,index=sortIx)
+    cItems=[sortIx] # initialize all items in one cluster
+    #print(cItems)
+    while len(cItems)>0:
+        cItems=[i[j:k] for i in cItems for j,k in ((0,len(i)//2), (len(i)//2,len(i))) if len(i)>1]
+        #print(cItems)# bi-section
+        for i in range(0,len(cItems),2): # parse in pairs
+            cItems0=cItems[i] # cluster 1
+            cItems1=cItems[i+1] # cluster 2
+            #print(cItems0, cItems1)
+            cVar0=getClusterVar(cov,cItems0)
+            cVar1=getClusterVar(cov,cItems1)
+            #print("Cluster Var: ",cVar0, cVar1)
+            alpha=1-cVar0/(cVar0+cVar1)
+            w[cItems0]*=alpha # weight 1
+            w[cItems1]*=1-alpha # weight 2
+    return w
+#------------------------------------------------------------------------------
+def correlDist(corr):
+    # A distance matrix based on correlation, where 0<=d[i,j]<=1
+    # This is a proper distance metric
+    dist=((1-corr)/2.)**.5 # distance matrix
+    return dist
+ 
+
+
+ def convert_to_mat(df_):
+    blank_ = np.zeros((len(TICKERS), len(TICKERS)), int)
+    np.fill_diagonal(blank_, 1)
+
+    for i in df
