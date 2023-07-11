22eca237cfc554ff135c5c18244f48c991680cb6
bkrayfield <blake.rayfield@nau.edu>
2023-07-11 11:16:27 -0700

    Optimizations

--- Summer2023_Remake/classhrp.py
+++ Summer2023_Remake/classhrp.py
@@ -16,110 +16,85 @@ class GenerateSIMMAT:
         self.API_KEY = API_KEY
         self.TICKERS = TICKERS
         self.YEARS = YEARS
+        self.queryApi = QueryApi(api_key=self.API_KEY)
+        self.ticker_str = ", ".join(map(str, self.TICKERS))
         
     def getFilingURLS(self):
         total_response = pd.DataFrame()
-        queryApi = QueryApi(api_key=self.API_KEY)
         for from_batch in range(0, 9800, 200): 
             payload = {
                 "query": {
                     "query_string": {
-                        "query": "ticker:({0}) AND formType:\"10-K\"".format(", ".join(map(str, self.TICKERS)))
+                        "query": f"ticker:({self.ticker_str}) AND formType:\"10-K\""
                     }
                 },
-                "from": "{0}".format(from_batch),
+                "from": f"{from_batch}",
                 "size": "200", # dont change this
                 "sort": [{ "filedAt": { "order": "desc" } }]
             }
-            response = queryApi.get_filings(payload)
-            ####print(len(response["filings"]))
-            if len(response["filings"]) == 0:
+            response = self.queryApi.get_filings(payload)
+            if not response["filings"]:
                 break
-            #print(pd.DataFrame.from_records(response['filings']))
-            temp_data = pd.DataFrame.from_records(response['filings'])
-            temp_data = temp_data.sort_values(['ticker','filedAt'], ascending = False)
+            temp_data = pd.DataFrame(response['filings'])
+            temp_data = temp_data.sort_values(['ticker','filedAt'], ascending=False)
             temp_data['fyear'] = pd.to_datetime(temp_data['periodOfReport'], errors='coerce').dt.year
-            #temp_data = temp_data[temp_data.fyear.isin(range(self.YEARS[0]-1,self.YEARS[1]+1))]
             temp_data['filedAt'] = pd.to_datetime(temp_data['filedAt'].str[:10])
             temp_data = temp_data.groupby(["ticker",'fyear']).head(1)
-            temp_data = temp_data.reset_index()
             total_response = pd.concat([total_response,temp_data])
-        return total_response
+        return total_response.reset_index()
     
     def get_SimilarityMAT(self, FILING_URLS):
-        data_ = []
-        for url in FILING_URLS:
-            payload = {
+        data_ = [rq.get("https://api.sec-api.io/extractor", params={
                 "token" : self.API_KEY,
                 "item": "1",
                 "url": url,
-                "type": "text"
-            }
-            RQ_URL = "https://api.sec-api.io/extractor"
-            request = rq.get(RQ_URL, params=payload)
-            data_.append(request.text)
+                "type": "text"}).text for url in FILING_URLS]
         vect = TfidfVectorizer(min_df=1, stop_words="english")
         tfidf = vect.fit_transform(data_)
-        return np.asarray((tfidf * tfidf.T).todense())
+        return (tfidf * tfidf.T).A #converts to numpy array, which is more efficient than todense()
     
     def get_Text_byTICandDate(self):
         temp_data = self.getFilingURLS()
         data_ = {x: {} for x in temp_data.ticker}
-        for index, url in temp_data.iterrows():
-            payload = {
-                "token" : self.API_KEY,
-                "item": "1",
-                "url": url.linkToFilingDetails,
-                "type": "text"
-            }
-            RQ_URL = "https://api.sec-api.io/extractor"
-            request = rq.get(RQ_URL, params=payload)
-            data_[url.ticker][url.filedAt] = request.text
+        for _, row in temp_data.iterrows():
+            data_[row.ticker][row.filedAt] = rq.get(
+                "https://api.sec-api.io/extractor",
+                params={
+                    "token" : self.API_KEY,
+                    "item": "1",
+                    "url": row.linkToFilingDetails,
+                    "type": "text"
+                }).text
         return data_
     
     def make_Timeline(self):
         data_ = self.get_Text_byTICandDate()
         all_ = self.process_data()
-        TICKERS = self.TICKERS
-        TICKERS.sort()
+        self.TICKERS.sort()
         vect = TfidfVectorizer(min_df=1, stop_words="english")
-        save_results = {}
-
-        for a in all_:
-            mat = [data_[tic][a[i]] for i, tic in enumerate(TICKERS)]
-            tfidf = vect.fit_transform(mat)
-            save_results[max(a)] = np.asarray((tfidf * tfidf.T).todense())
-
+        save_results = {max(a): (vect.fit_transform([data_[tic][a[i]] for i, tic in enumerate(self.TICKERS)]) * vect.fit_transform([data_[tic][a[i]] for i, tic in enumerate(self.TICKERS)]).T).A
+                        for a in all_}
         return save_results
     
     def create_simmat(self):
-        ###Primary Function
         save_results = self.make_Timeline()
-        TICKERS = self.TICKERS
-        TICKERS.sort()
-        parsed_data = []
-
-        for timestamp, array in save_results.items():
-            flattened_array = np.array(array).flatten()
-            parsed_data.append(flattened_array.tolist())
+        self.TICKERS.sort()
+        parsed_data = [np.array(array).flatten().tolist() for array in save_results.values()]
         
         df = pd.DataFrame(parsed_data)
         unclean = df.copy()
         unclean.index = save_results.keys()
-        unclean = unclean.sort_index()
-        unclean = unclean.replace(to_replace=0, method='ffill')
+        unclean.sort_index(inplace=True)
+        unclean.replace(to_replace=0, method='ffill', inplace=True)
 
         df = df.T.drop_duplicates().T
         df.index = save_results.keys()
-        df = df.sort_index()
-        df = df.replace(to_replace=0, method='bfill')
+        df.sort_index(inplace=True)
+        df.replace(to_replace=0, method='bfill', inplace=True)
 
-        #df.index = save_results.keys()
         df = df.loc[:, ~(df.sum(axis=0) >= len(df) - 1)]
-        #df.columns = [(TICKERS[y], TICKERS[x]) for y in range(len(TICKERS)) for x in range(y + 1, len(TICKERS))]
-        #df = df.sort_index()
 
-        return df, unclean 
+        return df, unclean
     
     def find_max_negative_index(self, lst):
         max_index = None
@@ -134,16 +109,10 @@ class GenerateSIMMAT:
 
     def process_data(self):
         temp_data = self.getFilingURLS()
-        li_ = temp_data.groupby(['ticker'])['filedAt'].apply(lambda x: x.tolist()).tolist()
+        li_ = temp_data.groupby(['ticker'])['filedAt'].apply(list).tolist()
 
-        idxs = []
-        for x in temp_data['filedAt'].unique():
-            keep = [self.find_max_negative_index([(z - x).days for z in y]) for y in li_]
-            if None not in keep:
-                idxs.append(keep)
+        idxs = [[self.find_max_negative_index([(z - x).days for z in y]) for y in li_] for x in temp_data['filedAt'].unique() if None not in [self.find_max_negative_index([(z - x).days for z in y]) for y in li_]]
 
-        idxs = np.array(idxs).T
-
-        all_ = [[li_[ip][i] for i in r] for ip, r in enumerate(idxs)]
+        all_ = [[li_[ip][i] for i in r] for ip, r in enumerate(np.array(idxs).T)]
 
         return np.array(all_).T
